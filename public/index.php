a29d3207dbcf389100972fdf0c8d6aec1647b33f
Matthew Baggett <matthew@fouroneone.us>
2013-10-23 16:21:01 +0100

    Adding default actions, htaccess

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 require_once("../configure.inc");
 require_once("../vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+require_once("../actions/info.inc");
+require_once("../actions/packages.json.inc");
+require_once("../actions/index.inc");
+
+$app->run();
